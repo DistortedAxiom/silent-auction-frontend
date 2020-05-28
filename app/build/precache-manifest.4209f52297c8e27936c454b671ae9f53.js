3ef67077a321877d671451fcecfd29139ad0c214
Anh Truong <aqtruong97@gmail.com>
2020-05-28 17:18:16 -0400

    commit

---
+++ app/build/precache-manifest.4209f52297c8e27936c454b671ae9f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68016fcb9944be92f5db42279557702",
+    "url": "/index.html"
+  },
+  {
+    "revision": "505e5267a615a4c01139",
+    "url": "/static/css/main.7bf0cf62.chunk.css"
+  },
+  {
+    "revision": "a846dc5662da55a1a1a7",
+    "url": "/static/js/2.38bd5170.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.38bd5170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505e5267a615a4c01139",
+    "url": "/static/js/main.a5501764.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  }
+]);
